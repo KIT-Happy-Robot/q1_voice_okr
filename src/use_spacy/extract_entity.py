429520e240ec85b4021242447a9e1a8f8a14a680
HppyKazuki <c1141120@planet.kanazawa-it.aac.jp>
2022-04-25 19:56:19 +0900

    update use_spacy

--- src/use_spacy/extract_entity.py
+++ src/use_spacy/extract_entity.py
@@ -4,18 +4,13 @@
 import rospy
 import spacy
 from os import path
+from spacy_preparation import preparation
 
-nlp = spacy.load('en_core_web_sm')
-
-file_path = path.expanduser("~/catkin_ws/src/q1_voice_okr/config/")
-
-def okuse_koya():
-    with open(file_path + "ex.txt", "r") as f:
-        for text in f:
-            text = text.replace("\n", "")
-            doc = nlp(text)
-            entity_list = ([(d.text, d.label_, spacy.explain(d.label_)) for d in doc.ents])
+def extractEntity():
+    docs = preparation()
+    for doc in docs:
+        entity_list = ([(d.text, d.label_, spacy.explain(d.label_)) for d in doc.ents])
     return entity_list
 
 if __name__ == "__main__":
-    print(okuse_koya())
+    print(extractEntity())
