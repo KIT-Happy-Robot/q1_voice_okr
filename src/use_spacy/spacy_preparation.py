c77b36264c433ce17a9bafe34fae553990e2a194
HppyKazuki <c1141120@planet.kanazawa-it.aac.jp>
2022-05-17 19:34:30 +0900

    update spacy

--- src/use_spacy/spacy_preparation.py
+++ src/use_spacy/spacy_preparation.py
@@ -2,21 +2,14 @@
 #-*- coding: utf-8 -*-
 
 import rospy
-from os import path
 import spacy
 
 nlp = spacy.load('en_core_web_sm')
 
-file_path = path.expanduser("~/catkin_ws/src/q1_voice_okr/config/")
-
-def preparation():
-    docs = []
-    with open(file_path + "ex.txt", "r") as f:
-        for text in f:
-            text = text.replace("\n", "")
-            doc = nlp(text)
-            docs.append(doc)
-    return docs
+def preparation(text):
+    doc = nlp(text)
+    return doc
 
 if __name__ == "__main__":
-    print(preparation())
+    text = input("英文を入力>> ")
+    print(preparation(text))
