dd8a2760e08e7bd57924d0bf0b9a7aa9a535947f
HappyKawara <c1139136@planet.kanazawa-it.ac.jp>
2022-05-25 18:28:23 +0900

    update use_spacy

---
+++ src/use_spacy/spacy_preparation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import rospy
+import spacy
+
+nlp = spacy.load('en_core_web_md')
+
+def preparation(text):
+    doc = nlp(text)
+    return doc
+
+if __name__ == "__main__":
+    text = input("英文を入力>> ")
+    print(preparation(text))
