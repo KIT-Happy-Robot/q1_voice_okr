5eeb9a843b376ba8c2ec805a63f8854d7b6a5b57
happykoya <c1103809@planet.kanazawa-it.ac.jp>
2022-05-26 20:24:47 +0900

    update src2

--- src/attention/ex/sentencesplit.py
+++ src/attention/ex/sentencesplit.py
@@ -1,28 +1,46 @@
 # -*- coding: utf-8 -*-
-sentence = "pleaseescort{name}tothe{location},youwillfindhimatthe{location}"
+sentence = "please escort name to the location and you will find him at the location"
 str = "Please escort {name} to the {location}, you will find him at the {location}"
 str_out = "escort{name}{location}Nonefindhim{location}None"
 sentence_ls=[]
-str_ls = str_out.split()
-print(str_ls)
+str_ls = sentence.split()
+# print(str_ls)
 
-delimiter_ls=[i for i,x in enumerate(str_ls) if "," in x or "and" in x]
-for i,num in enumerate(delimiter_ls):
-    if i==0:
-        if "and" in str_ls[num]:
-            sentence_ls.append(str_ls[:num])
-        else:
-            str_ls[num]=str_ls[num].replace(",","")
-            sentence_ls.append(str_ls[:num+1])
+# delimiter_ls=[i for i,x in enumerate(str_ls) if "," in x or "and" in x]
+delimiter_ls = 0
+for i,x in enumerate(str_ls):
+    if "," in x or "and" in x:
+        delimiter_ls = i 
+
+for i,num in enumerate(str_ls):
+    if "and" in str_ls[i]:
+        sentence_ls.append(str_ls[:i])
+        sentence_ls.append(str_ls[i+1:])
+
+    elif "," in str_ls[i]:
+        sentence_ls.append(str_ls[:i])
+        sentence_ls.append(str_ls[i:])
+
     else:
-        str_ls[num]=str_ls[num].replace(",","")
-        if len(delimiter_ls)-1 != i:
-            sentence_ls.append(str_ls[num+1:delimiter_ls[i+1]])
-        else:
-            sentence_ls.append(str_ls[num+1:])
+        continue
+    # if i == 0:
+        # if "and" in str_ls[i]:
+            # sentence_ls.append(str_ls[:i])
+        
+        # else:
+            # str_ls[i]=str_ls[i].replace(",","")
+            # sentence_ls.append(str_ls[:i+1])
+            
+    # else:
+        # str_ls[i]=str_ls[i].replace(",","")
+        # if delimiter_ls-1 != i:
+            # sentence_ls.append(str_ls[i+1:])
+            
+        # else:
+            # sentence_ls.append(str_ls[i+1:])
 
 print(sentence_ls)
-
+# print(delimiter_ls)
 sub_ls=[]
 for i,word in enumerate(str_ls):
     sub_ls.append(word)
@@ -30,4 +48,4 @@ for i,word in enumerate(str_ls):
         sentence_ls.append(sub_ls)
         sub_ls=[]
 
-print(sub_ls)
+# print(sub_ls)
