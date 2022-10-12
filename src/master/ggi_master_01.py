4d5431df6d43bee9fa599a208608dab8c02c48ed
HappyKawara <c1139136@planet.kanazawa-it.ac.jp>
2022-10-12 19:39:41 +0900

    add: first_tutorial

--- src/master/ggi_master_01.py
+++ src/master/ggi_master_01.py
@@ -28,21 +28,26 @@ stt_pub = rospy.ServiceProxy('/stt_server', SpeechToText)
 
 test_phase_00 = rospy.ServiceProxy('/ggi_learning',GgiLearning)
 
+
+plase = "initial_position"
+
 #Traning_Phase
 class Move_First(smach.State):#初期位置まで移動
     def __init__(self):
         smach.State.__init__(self, outcomes = ['move_first_finish'])
+        self.set = rospy.ServiceProxy('/set_location', SetLocation)
 
     def execute(self, state):
         rospy.loginfo('Executing state: MOVE_FIRST')
         enter_room = rospy.ServiceProxy('/enter_room_server', EnterRoom)
         enter_room(1.0, 0.5)
+        self.set("state: 'add' name: '{}'".format(plase))
         return 'move_first_finish'
 
 class Chaser (smach.State):#物体がある位置まで移動
     def __init__(self):
         smach.State.__init__(self, outcomes = ['chaser_finish'],
-                                   input_keys = ['PASS_count_in'],
+                                   input_keys = ['PASS_count'],
                                    output_keys = ['PASS_count_out'])
         self.chaser_pub = rospy.Publisher('/follow_human', String, queue_size = 1)
         rospy.Subscriber('/find_str', String, self.findCB)
@@ -71,6 +76,8 @@ class Chaser (smach.State):#物体がある位置まで移動
                 self.find_msg = 'lost_stop'
                 self.strat_time = time.time()
             elif self.cmd_sub == 0.0 and self.find_msg == 'NULL':
+        self.plase_name = str(self.a()).split(": ")
+
                 tts_pub('Is this the location of the object')
                 count = False
                 while count:
@@ -127,17 +134,17 @@ class Register (smach.State):#場所と物の登録
     def execute(self, userdata):
         count = userdata.count_in
         rospy.loginfo('Executing state: REGISTER')
-        plase =  self.learning().split('"')
-        plase_name = plase[1]
-        if plase_name:
+        self.plase =  self.learning().split('"')
+        self.plase_name = self.plase[1]
+        if self.plase_name:
             if count < 2:
                 count += 1
                 userdata.count_out = count
                 self.set = rospy.ServiceProxy('/set_location', SetLocation)
-                self.set("state: 'add' name: {}".format(plase_name))
+                self.set("state: 'add' name: '{}'".format(plase_name))
                 if count == 2:
                     self.save = rospy.ServiceProxy('/set_location', SetLocation)
-                    self.save(("state: 'save' name: 'ggi_location.yaml'")
+                    self.save("state: 'save' name: 'ggi_location.yaml'")
                 return 'register_success'
             else:
                 return 'finish_traning'
@@ -146,28 +153,28 @@ class Register (smach.State):#場所と物の登録
 
 #Test_Phase
 
-class Get_Plase(smach.State):#命令文を読み解く
+class Get_Plase(smach.State):#命令文を読み解く,物体がある場所まで移動して掴む
     def __init__(self):
-        smach.State.__init__(self, outcomes = ['get_plase_success'],
-                                   input_keys = [''],
-                                   output_keys = [''])
+        smach.State.__init__(self, outcomes = ['get_plase_success',
+                                               'get_plase_failure'])
         self.a = rospy.ServiceProxy('test_phase',GgiLearning)
+        self.get_ob = rospy.ServiceProxy('/recognition_to_grasping', StrInt)
+        self.plase_name = str(self.a()).split(": ")
+        self.target_name = "any"
+
     def execute(self, state):
         rospy.loginfo('Executing state: GET_PLASE')
-        self.a()
-        return 'get_plase_success'
-
-
-class Get_Object(smach.State):#物体がある場所まで移動して掴む
-    def __init__(self):
-        smach.State.__init__(self, outcomes = ['get_object_success',
-                                               'get_object_failure'])
-    def execute(self, state):
-        rospy.loginfo('Executing state:GET_OBJECT')
-        if True:
-            return 'get_object_success'
+        self.plase_name = str(self.a()).split(": ")
+        self.file_path = path.expanduser("~/catkin_ws/src/q1_voice_okr/config/")
+        self.navi = rospy.ServiceProxy('/navi_location', SetLocation)
+        if bool(self.navi(self.plase_name)):
+            if bool(self.get_ob(self.target_name)):
+                return 'get_plase_success'
+            else:
+                return 'get_plase_failure'
         else:
-            return 'get_object_failure'
+            return 'get_plase_failure'
+
 
 class Return(smach.State):#オペレーターの位置まで帰る
     def __init__(self):
@@ -175,19 +182,19 @@ class Return(smach.State):#オペレーターの位置まで帰る
                                                'ggi_finish'],
                                     input_keys = ['count_in'],
                                     output_keys = ['count_out'])
+        self.navi = rospy.ServiceProxy('/navi_location', SetLocation)
 
     def execute(self, userdata):
+        rospy.loginfo("Executing state: RETURN")
         count = userdata.count_in
         if count < 4:
+            if bool(self.navi("state: 'add' name: '{}'".format(plase)))
             count += 1
             userdata.count_out = count
             return 'return_finish'
         else:
             tts_pub('Finish go get it. Thank you very much.')
             return 'ggi_finish'
-
-
-
 
 if __name__ == '__main__':
     rospy.init_node('ggi_master')
@@ -203,8 +210,6 @@ if __name__ == '__main__':
                 transitions = {'move_first_finish':'CHASER'},
                 remapping = {'count_in':'count',
                              'count_out':'count'})
-
-
 
         smach.StateMachine.add(
                 'CHASER',
@@ -225,17 +230,8 @@ if __name__ == '__main__':
         smach.StateMachine.add(
                 'GET_PLASE',
                 Get_Plase(),
-                transitions = {'get_plase_success':'GET_OBJECT'},
-                remapping = {'':'',
-                             '':''})
-
-        smach.StateMachine.add(
-                'GET_OBJECT',
-                Get_Object(),
-                transitions = {'get_object_success':'RETURN',
-                               'get_object_failure':'GET_OBJECT'},
-                remapping = {'':'',
-                             '':''})
+                transitions = {'get_plase_success':'RETURN'
+                               'get_plase_failure':'RETURN'},
 
         smach.StateMachine.add(
                 'RETURN',
@@ -244,6 +240,5 @@ if __name__ == '__main__':
                                'ggi_finish':'finish_sm_top'},
                 remapping = {'count_in':'count',
                              'count_out':'count'})
-
 
         outcomes = sm_top.execute()
---
+++ src/master/rosservice.py
@@ -0,0 +1,6 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+
+ggi_learning = rospy
